4349e4aa5dfe84edaa79291e22a9994ea9a92069
Stephanie Feddern <s.feddern@gmail.com>
2017-06-01 19:17:34 +0200

    finetuning on gulpjs file * uglify javascript * rename minified files with suffix .min

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
 var gulp = require('gulp'),
-    plumber = require('gulp-plumber'),
+    babel = require('gulp-babel'),
     cleanCSS = require('gulp-clean-css'),
-    sass = require('gulp-sass');
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify');;
 
 gulp.task('default', function() {
   gulp.start('watch');
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', ['minify-css', 'minify-js'], function() {
   gulp.watch('./assets/css/**/*.scss', ['minify-css']);
+  gulp.watch('./assets/javascript/**/*.js', ['minify-js']);
 });
 
 gulp.task('minify-css', function() {
@@ -19,5 +23,17 @@ gulp.task('minify-css', function() {
       console.log(details.name + ' before minification: ' + details.stats.originalSize);
       console.log(details.name + ' after minification: ' + details.stats.minifiedSize);
     }))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('./assets/css/'));
+});
+
+gulp.task('minify-js', function() {
+  gulp.src('./assets/javascript/application.js')
+    .pipe(plumber())
+    .pipe(babel({
+        presets: ['es2015']
+    }))
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./assets/javascript/'));
 });
